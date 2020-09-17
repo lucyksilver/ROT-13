5afda3cd831421f13ff88fe781c517bcd7c7433a
Lucy Silver <37183799+lucyksilver@users.noreply.github.com>
2020-09-17 12:31:13 +0100

    Update interface.rb

--- interface.rb
+++ interface.rb
@@ -1,8 +1,24 @@
 require_relative 'rot-13.rb'
 
-puts 'Hello and welcome, please enter a sentence'
-message = gets.chomp
-puts 'Please enter a number'
-number = gets.chomp.to_i
-answer = number_mix(message, number)
-puts "Your new sentence is #{answer}"
+puts 'Hello and welcome!'
+
+answer = nil
+
+until answer == "n"
+  puts 'Please enter a word or phrase'
+  print "> "
+  message = gets.chomp
+  puts 'Please enter a number between 1 and 25'
+  print "> "
+  number = gets.chomp.to_i
+  if number > 25
+    puts 'Try again! Your number must be between 1 and 25'
+    print "> "
+    number = gets.chomp.to_i
+  end
+  answer = number_mix(message, number)
+  puts "Your newly jazzed up phrase is #{answer}, would you like to play again? Y / N"
+  answer = gets.chomp.downcase
+end
+
+puts "Thank you, goodbye!"
